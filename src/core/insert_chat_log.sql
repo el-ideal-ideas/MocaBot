1ef6f80f777f6b750e384c21d90525c2569dbccd
el-ideal-ideas <el.idealideas@gmail.com>
2020-12-21 14:45:05 +0900

    Update

--- src/core/insert_chat_log.sql
+++ src/core/insert_chat_log.sql
@@ -1,3 +1,3 @@
 insert into `[el]#moca_prefix#chat_logs`(
   ip, bot_id, user_msg, bot_msg, time, res_type, client_id
-) values (%s, %s, %s, %s, %s, now(), %s);
+) values (%s, %s, %s, %s, now(), %s, %s);
